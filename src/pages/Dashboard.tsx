70e1bcb2373bfcbab2719a43facb16b052310202
Emmanuel <ema.araujo.ea@gmail.com>
2025-01-31 19:28:40 -0300

    background

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,7 +4,7 @@ export const Dashboard = () => {
     return (
         <div className='w-full h-full'>
             <Navbar />
-            <div className='w-full h-[calc(100vh-83px)] flex bg-gray-300'>
+            <div className='w-full h-[calc(100vh-83px)] flex bg-gray-100'>
                 <Sidebar />
                 <div className='w-full sm:max-w-[480px] md:max-w-[768px] lg:max-w-[1024px] mx-auto p-4 '>
                     <Content />
